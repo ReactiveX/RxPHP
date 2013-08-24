495437162a7c3ed3c5e94f65aa4ca9fc59b23a1b
Alexander <iam.asm89@gmail.com>
2013-08-24 21:41:19 +0200

    Unbreak the build on php 5.3. Fix calling 'array' callbacks
    Probably related to: https://bugs.php.net/bug.php?id=47160
    
    Revert if 5.3 is EOL. ;)

--- lib/Rx/Observer/CallbackObserver.php
+++ lib/Rx/Observer/CallbackObserver.php
@@ -20,20 +20,18 @@ class CallbackObserver extends AbstractObserver
 
     protected function completed()
     {
-        $onCompleted= $this->onCompleted;
-        $onCompleted();
+        // Cannot use $foo() here, see: https://bugs.php.net/bug.php?id=47160
+        call_user_func($this->onCompleted);
     }
 
     protected function error(Exception $error)
     {
-        $onError = $this->onError;
-        $onError($error);
+        call_user_func_array($this->onError, array($error));
     }
 
     protected function next($value)
     {
-        $onNext = $this->onNext;
-        $onNext($value);
+        call_user_func_array($this->onNext, array($value));
     }
 
     private function getOrDefault($callback, $default)
