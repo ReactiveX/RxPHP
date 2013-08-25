3048789e2e91d3a35ab944ccd41c52cb6edcec75
Alexander <iam.asm89@gmail.com>
2013-08-25 21:56:32 +0200

    More tests for AutoDetachObserver

--- test/Rx/Observer/AutoDetachObserverTest.php
+++ test/Rx/Observer/AutoDetachObserverTest.php
@@ -37,4 +37,55 @@ class AutoDetachObserverTest extends TestCase
         $observer->onError(new Exception());
         $this->assertTrue($disposed);
     }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage fail
+     */
+    public function it_disposes_if_observer_on_completed_throws()
+    {
+        $disposed   = false;
+        $disposable = new CallbackDisposable(function() use (&$disposed){ $disposed = true; });
+
+        $observer   = new AutoDetachObserver(new CallbackObserver(null, null, function() { throw new Exception('fail'); }));
+        $observer->setDisposable($disposable);
+
+        $observer->onCompleted();
+        $this->assertTrue($disposed);
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage fail
+     */
+    public function it_disposes_if_observer_on_error_throws()
+    {
+        $disposed   = false;
+        $disposable = new CallbackDisposable(function() use (&$disposed){ $disposed = true; });
+
+        $observer   = new AutoDetachObserver(new CallbackObserver(null, function() { throw new Exception('fail'); }));
+        $observer->setDisposable($disposable);
+
+        $observer->onError(new Exception());
+        $this->assertTrue($disposed);
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage fail
+     */
+    public function it_disposes_if_observer_on_next_throws()
+    {
+        $disposed   = false;
+        $disposable = new CallbackDisposable(function() use (&$disposed){ $disposed = true; });
+
+        $observer   = new AutoDetachObserver(new CallbackObserver(function() { throw new Exception('fail'); }));
+        $observer->setDisposable($disposable);
+
+        $observer->onNext(42);
+        $this->assertTrue($disposed);
+    }
 }
