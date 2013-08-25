47bae83d2c6caa4b50e7a5fbc04c741f9fa60c18
Alexander <iam.asm89@gmail.com>
2013-08-25 21:46:37 +0200

    More tests for RefCountDisposable

--- test/Rx/Disposable/RefCountDisposableTest.php
+++ test/Rx/Disposable/RefCountDisposableTest.php
@@ -62,6 +62,82 @@ class RefCountDisposableTest extends TestCase
         $d2->dispose();
         $this->assertTrue($d->isDisposed());
     }
+
+    /**
+     * @test
+     */
+    public function it_does_not_dispose_the_primary_if_refcount_inner_disposable_is_disposed_multiple_times()
+    {
+        $d = new BooleanDisposable();
+        $r = new RefCountDisposable($d);
+
+        $d1 = $r->getDisposable();
+        $d2 = $r->getDisposable();
+
+        $d1->dispose();
+        $this->assertFalse($d->isDisposed());
+
+        $d1->dispose();
+        $this->assertFalse($d->isDisposed());
+
+        $r->dispose();
+        $this->assertFalse($d->isDisposed());
+
+        $d2->dispose();
+        $this->assertTrue($d->isDisposed());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_dispose_the_primary_if_already_disposed_via_refcount()
+    {
+        $called = 0;
+        $d = new CallbackDisposable(function() use (&$called) { $called++; });
+        $r = new RefCountDisposable($d);
+
+        $r->dispose();
+        $this->assertEquals(1, $called);
+        $r->dispose();
+        $this->assertEquals(1, $called);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_dispose_the_primary_if_already_disposed()
+    {
+        $called = 0;
+        $d = new CallbackDisposable(function() use (&$called) { $called++; });
+        $r = new RefCountDisposable($d);
+
+        $d1 = $r->getDisposable();
+
+        $d1->dispose();
+        $r->dispose();
+        $this->assertEquals(1, $called);
+
+        $d1->dispose();
+        $this->assertEquals(1, $called);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_noop_disposable_if_primary_is_already_disposed()
+    {
+        $called = 0;
+        $d = new CallbackDisposable(function() use (&$called) { $called++; });
+        $r = new RefCountDisposable($d);
+
+        $r->dispose();
+        $this->assertEquals(1, $called);
+
+        $d1 = $r->getDisposable();
+        $d1->dispose();
+        $r->dispose();
+        $this->assertEquals(1, $called);
+    }
 }
 
 class BooleanDisposable implements DisposableInterface
