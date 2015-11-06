1b8c65e7fa6ffb5686ad6281230dad3018f13a6a
David Dan <davidwdan@gmail.com>
2015-11-06 16:42:28 -0500

    Replaced select and selectMany with map and flatMap Added aliases for select and selectMany

---
+++ lib/Rx/Operator/MapOperator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Rx\Operator;
+
+use Rx\ObservableInterface;
+use Rx\Observer\CallbackObserver;
+use Rx\ObserverInterface;
+use Rx\SchedulerInterface;
+
+class MapOperator implements OperatorInterface
+{
+    /** @var callable  */
+    private $selector;
+
+    /**
+     * MapOperator constructor.
+     * @param $selector
+     */
+    public function __construct($selector)
+    {
+        if ( ! is_callable($selector)) {
+            throw new \InvalidArgumentException('Selector should be a callable.');
+        }
+
+        $this->selector = $selector;
+    }
+
+    /**
+     * @param \Rx\ObservableInterface $observable
+     * @param \Rx\ObserverInterface $observer
+     * @param \Rx\SchedulerInterface $scheduler
+     * @return \Rx\DisposableInterface
+     */
+    public function __invoke(ObservableInterface $observable, ObserverInterface $observer, SchedulerInterface $scheduler = null)
+    {
+        $selectObserver = new CallbackObserver(
+          function($nextValue) use ($observer) {
+              $value = null;
+              try {
+                  $value = call_user_func($this->selector, $nextValue);
+              } catch (\Exception $e) {
+                  $observer->onError($e);
+              }
+              $observer->onNext($value);
+          },
+          function($error) use ($observer) {
+              $observer->onError($error);
+          },
+          function() use ($observer) {
+              $observer->onCompleted();
+          }
+        );
+
+        return $observable->subscribe($selectObserver, $scheduler);
+    }
+}
