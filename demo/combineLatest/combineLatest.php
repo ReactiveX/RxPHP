01549b7629fc90e3f471b5e6713a9769fbef9164
David Dan <davidwdan@gmail.com>
2016-01-04 19:44:28 -0500

    Added combineLatest demo

---
+++ demo/combineLatest/combineLatest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+$loop = \React\EventLoop\Factory::create();
+$scheduler = new \Rx\Scheduler\EventLoopScheduler($loop);
+
+$source1 = \Rx\Observable\BaseObservable::fromArray(range(0, 100));
+$source2 = \Rx\Observable\BaseObservable::fromArray(range(0, 100));
+
+$source = $source1->combineLatest([$source2], function($value1, $value2){
+    return "First: {$value1}, Second: {$value2}";
+})->take(4);
+
+$subscription = $source->subscribe($stdoutObserver, $scheduler);
+
+$loop->run();
