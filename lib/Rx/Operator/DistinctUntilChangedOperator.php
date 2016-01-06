a5703d114ac221e5d52280b5b268258f334f4a04
David Dan <davidwdan@gmail.com>
2016-01-06 11:26:22 -0500

    Pass scheduler through distinctUntilChanged operator

--- lib/Rx/Operator/DistinctUntilChangedOperator.php
+++ lib/Rx/Operator/DistinctUntilChangedOperator.php
@@ -25,47 +25,47 @@ class DistinctUntilChangedOperator implements OperatorInterface
 
     }
 
-
     public function __invoke(ObservableInterface $observable, ObserverInterface $observer, SchedulerInterface $scheduler = null)
     {
 
         $hasCurrentKey = false;
         $currentKey    = null;
+        $cbObserver    = new CallbackObserver(
+            function ($value) use ($observer, &$hasCurrentKey, &$currentKey) {
+                $key = $value;
+                if ($this->keySelector) {
+                    try {
+                        $key = call_user_func($this->keySelector, $value);
+                    } catch (\Exception $e) {
+                        return $observer->onError($e);
+                    }
+                }
 
-        return $observable->subscribe(new CallbackObserver(
-          function ($value) use ($observer, &$hasCurrentKey, &$currentKey) {
-              $key = $value;
-              if ($this->keySelector) {
-                  try {
-                      $key = call_user_func($this->keySelector, $value);
-                  } catch (\Exception $e) {
-                      return $observer->onError($e);
-                  }
-              }
+                $comparerEquals = null;
+                if ($hasCurrentKey) {
+                    try {
+                        $comparerEquals = call_user_func($this->comparer, $currentKey, $key);
+                    } catch (\Exception $e) {
+                        return $observer->onError($e);
+                    }
+                }
 
-              $comparerEquals = null;
-              if ($hasCurrentKey) {
-                  try {
-                      $comparerEquals = call_user_func($this->comparer, $currentKey, $key);
-                  } catch (\Exception $e) {
-                      return $observer->onError($e);
-                  }
-              }
+                if (!$hasCurrentKey || !$comparerEquals) {
+                    $hasCurrentKey = true;
+                    $currentKey    = $key;
+                    $observer->onNext($value);
+                }
 
-              if (!$hasCurrentKey || !$comparerEquals) {
-                  $hasCurrentKey = true;
-                  $currentKey    = $key;
-                  $observer->onNext($value);
-              }
+            },
+            function ($e) use ($observer) {
+                $observer->onError($e);
+            },
+            function () use ($observer) {
+                $observer->onCompleted();
+            }
+        );
 
-          },
-          function ($e) use ($observer) {
-              $observer->onError($e);
-          },
-          function () use ($observer) {
-              $observer->onCompleted();
-          }));
+        return $observable->subscribe($cbObserver, $scheduler);
 
     }
-
 }
