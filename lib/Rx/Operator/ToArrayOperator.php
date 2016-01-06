d702c71c88c8472a171ec20cc151532fb92cadb0
David Dan <davidwdan@gmail.com>
2016-01-06 11:42:38 -0500

    Pass scheduler through toArray operator

--- lib/Rx/Operator/ToArrayOperator.php
+++ lib/Rx/Operator/ToArrayOperator.php
@@ -1,37 +1,35 @@
 <?php
 
-
 namespace Rx\Operator;
 
-use Rx\Disposable\EmptyDisposable;
 use Rx\ObservableInterface;
 use Rx\Observer\CallbackObserver;
 use Rx\ObserverInterface;
 use Rx\SchedulerInterface;
-
 
 class ToArrayOperator implements OperatorInterface
 {
     /** @var array  */
     private $arr = [];
 
-
     /**
      * @inheritDoc
      */
     public function __invoke(ObservableInterface $observable, ObserverInterface $observer, SchedulerInterface $scheduler = null)
     {
-        return $observable->subscribe(new CallbackObserver(
+        $cbObserver = new CallbackObserver(
             function ($x) {
                 $this->arr[] = $x;
             },
             function ($e) use ($observer) {
                 $observer->onError($e);
             },
-            function() use ($observer) {
+            function () use ($observer) {
                 $observer->onNext($this->arr);
                 $observer->onCompleted();
             }
-        ));
+        );
+
+        return $observable->subscribe($cbObserver, $scheduler);
     }
 }
