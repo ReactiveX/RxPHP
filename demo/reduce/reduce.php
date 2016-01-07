b7013f9e34613f949640a89ba9eb84a53be6b70e
David Dan <davidwdan@gmail.com>
2016-01-07 10:25:17 -0500

    Added reduce demo

---
+++ demo/reduce/reduce.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+//Without a seed
+$source = \Rx\Observable\BaseObservable::fromArray(range(1, 3));
+
+$subscription = $source
+    ->reduce(function ($acc, $x) {
+        return $acc + $x;
+    })
+    ->subscribe($createStdoutObserver());
+
+//Next value: 6
+//Complete!
+
+
+//With a seed
+$source = \Rx\Observable\BaseObservable::fromArray(range(1, 3));
+
+$subscription = $source
+    ->reduce(function ($acc, $x) {
+        return $acc * $x;
+    }, 1)
+    ->subscribe($createStdoutObserver());
+
+//Next value: 6
+//Complete!
