ac146154e1d7447342280df43fc15246c8334d2c
David Dan <davidwdan@gmail.com>
2016-01-07 09:41:07 -0500

    Added toArray demo

---
+++ demo/toArray/toArray.php
@@ -0,0 +1,22 @@
+<?php
+
+use Rx\Observer\CallbackObserver;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+$source = \Rx\Observable\BaseObservable::fromArray([1, 2, 3, 4]);
+
+$observer = $createStdoutObserver();
+
+$subscription = $source->toArray()
+    ->subscribe(new CallbackObserver(
+        function ($array) use ($observer) {
+            $observer->onNext(json_encode($array));
+        },
+        [$observer, "onError"],
+        [$observer, "onCompleted"]
+    ));
+
+
+//Next value: [1,2,3,4]
+//Complete!
