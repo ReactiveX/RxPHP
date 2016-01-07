8de52a38b0f2e01f117d5af633dc1a8dca2d9f86
matt <matt@bonneau.net>
2016-01-06 22:45:44 -0500

    Fix ReturnObservable to override subscribe instead of doStart

--- lib/Rx/Observable/ReturnObservable.php
+++ lib/Rx/Observable/ReturnObservable.php
@@ -2,6 +2,11 @@
 
 namespace Rx\Observable;
 
+use Rx\Disposable\CompositeDisposable;
+use Rx\ObserverInterface;
+use Rx\Scheduler\ImmediateScheduler;
+use Rx\SchedulerInterface;
+
 class ReturnObservable extends BaseObservable
 {
     private $value;
@@ -14,21 +19,27 @@ class ReturnObservable extends BaseObservable
         $this->value = $value;
     }
 
-    protected function doStart($scheduler)
+    public function subscribe(ObserverInterface $observer, SchedulerInterface $scheduler = null)
     {
         $value     = $this->value;
-        $observers = &$this->observers;
 
-        $scheduler->schedule(function () use (&$observers, $value) {
-            foreach ($observers as $observer) {
-                $observer->onNext($value);
-            }
-        });
+        $scheduler = $scheduler ?: new ImmediateScheduler();
 
-        $scheduler->schedule(function () use (&$observers) {
-            foreach ($observers as $observer) {
-                $observer->onCompleted();
-            }
-        });
+        $disposable = new CompositeDisposable();
+
+        $disposable->add($scheduler->schedule(function () use ($observer, $value) {
+            $observer->onNext($value);
+        }));
+
+        $disposable->add($scheduler->schedule(function () use ($observer) {
+            $observer->onCompleted();
+        }));
+
+        return $disposable;
+    }
+
+    protected function doStart($scheduler)
+    {
+
     }
 }
