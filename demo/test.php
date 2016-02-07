883ad4ba8bfeb3e2192d7501039b99eb8a641229
Alexander <iam.asm89@gmail.com>
2016-02-07 22:25:17 +0000

    Add ability to record and validate output of demo files
    Over time the RxPHP project has added demos for various observables and
    operators. From time to time the demos tend to break because of
    refactoring or other changes.
    
    This commit introduces a "test" command inside the demo folder that will
    find demos that have an accompanying `.expect` file and ensure that
    running the demo yields the same output as is expected. This will ensure
    that demos keep working, but it also turns each demo into an end to end
    tests for the RxPHP project.
    
    Example usage:
    
    ```
    $ php demo/test.php
    Found 2 demos with an expect file.
    [v] demo/combineLatest/combineLatest.php
    [v] demo/concat/concat.php
    ```

---
+++ demo/test.php
@@ -0,0 +1,56 @@
+<?php
+
+function run_cmd($file) {
+    $output = [];
+    $exit_code = -1;
+    exec(
+        PHP_BINARY . ' ' . $file,
+        $output,
+        $exit_code
+    );
+    return array($exit_code, implode("\n", $output));
+}
+
+function run_demo($file) {
+    list($exit_code, $output) = run_cmd($file);
+    if ($exit_code != 0) {
+        return false;
+    }
+    $expected = file_get_contents($file . '.expect');
+    return trim($output) == trim($expected);
+}
+
+function find_demos($dir) {
+    return glob($dir . '/*/*.php');
+}
+
+function has_expect($file) {
+    return file_exists($file . '.expect');
+}
+
+function strip_cwd($cwd, $absolute_path) {
+    $is_prefix = 0 === strpos($absolute_path, $cwd);
+    if (!$is_prefix) {
+        return $absolute_path;
+    }
+    return substr($absolute_path, strlen($cwd) + 1 /* strip trailing / */);
+}
+
+function main($cwd) {
+    $demos = array_filter(find_demos(__DIR__), 'has_expect');
+
+    echo sprintf("Found %d demos with an expect file.\n", count($demos));
+
+    $overall_success = true;
+    foreach ($demos as $demo) {
+        $result = run_demo($demo);
+        $overall_success = $overall_success && $result;
+
+        $readable = $result ? 'v' : 'x';
+        echo sprintf("[%s] %s\n", $readable, strip_cwd($cwd, $demo));
+    }
+
+    return $overall_success ? 0 : 1;
+}
+
+exit(main(getcwd()));
