8b285b33f79a0b540a7e3ec0af4624ec9d82bc34
Matt Bonneau <matt@bonneau.net>
2016-04-06 17:41:55 -0400

    Testing for materialize

--- test/Rx/Functional/Operator/MaterializeTest.php
+++ test/Rx/Functional/Operator/MaterializeTest.php
@@ -3,13 +3,54 @@
 namespace Rx\Functional\Operator;
 
 use Rx\Functional\FunctionalTestCase;
+use Rx\Notification;
 use Rx\Notification\OnCompletedNotification;
+use Rx\Notification\OnErrorNotification;
 use Rx\Notification\OnNextNotification;
 use Rx\Observable;
 
 class MaterializeTest extends FunctionalTestCase
 {
-    public function testMaterializeNever()
+    private function getNotificationValue(Notification $notification)
+    {
+        if ($notification instanceof OnCompletedNotification) {
+            throw new \Exception("Unable to get value of OnCompletedNotification");
+        }
+
+        $value = null;
+        $valueGrabber = function ($v) use (&$value) {
+            $value = $v;
+        };
+
+        $notification->accept($valueGrabber, $valueGrabber);
+        
+        return $value;
+    }
+    
+    public function materializedNotificationsEqual(Notification $a, Notification $b)
+    {
+        $aValue = $this->getNotificationValue($a);
+        $bValue = $this->getNotificationValue($b);
+        
+        if (get_class($aValue) !== get_class($bValue)) {
+            return false;
+        }
+        
+        if (!($aValue instanceof Notification)) {
+            return false;
+        }
+        
+        if ($aValue instanceof OnCompletedNotification) {
+            return true;
+        }
+        
+        return $this->getNotificationValue($aValue) === $this->getNotificationValue($bValue);
+    }
+
+    /**
+     * @test
+     */
+    public function materialize_never()
     {
         $results = $this->scheduler->startWithCreate(function () {
             return Observable::never()->materialize();
@@ -18,7 +59,10 @@ class MaterializeTest extends FunctionalTestCase
         $this->assertMessages([], $results->getMessages());
     }
 
-    public function testMaterializeEmpty()
+    /**
+     * @test
+     */
+    public function materialize_empty()
     {
         $xs = $this->createHotObservable([
             onNext(150, 1),
@@ -30,8 +74,129 @@ class MaterializeTest extends FunctionalTestCase
         });
 
         $this->assertMessages([
-            onNext(250, new OnNextNotification('123')),
+            onNext(250, new OnCompletedNotification(), [$this, 'materializedNotificationsEqual']),
             onCompleted(250)
+        ], $results->getMessages());
+    }
+
+    /**
+     * @test
+     */
+    public function materialize_return()
+    {
+        $xs = $this->createHotObservable([
+            onNext(150, 1),
+            onNext(210, 2),
+            onCompleted(250)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->materialize();
+        });
+
+        $this->assertMessages([
+            onNext(210, new OnNextNotification(2), [$this, 'materializedNotificationsEqual']),
+            onNext(250, new OnCompletedNotification(), [$this, 'materializedNotificationsEqual']),
+            onCompleted(250)
+        ], $results->getMessages());
+    }
+
+    /**
+     * @test
+     */
+    public function materialize_throw()
+    {
+        $error = new \Exception();
+
+        $xs = $this->createHotObservable([
+            onNext(150, 1),
+            onError(250, $error)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->materialize();
+        });
+
+        $this->assertMessages([
+            onNext(250, new OnErrorNotification($error), [$this, 'materializedNotificationsEqual']),
+            onCompleted(250)
+        ], $results->getMessages());
+    }
+
+    /**
+     * @test
+     * @requires function Rx\Observable::dematerialize
+     */
+    public function materialize_dematerialize_never()
+    {
+        $results = $this->scheduler->startWithCreate(function () {
+            return Observable::never()->materialize()->dematerialize();
+        });
+
+        $this->assertMessages([], $results->getMessages());
+    }
+
+    /**
+     * @test
+     * @requires function Rx\Observable::dematerialize
+     */
+    public function materialize_dematerialize_empty()
+    {
+        $xs = $this->createHotObservable([
+            onNext(150, 1),
+            onCompleted(250)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->materialize()->dematerialize();
+        });
+
+        $this->assertMessages([
+            onCompleted(250)
+        ], $results->getMessages());
+    }
+
+    /**
+     * @test
+     * @requires function Rx\Observable::dematerialize
+     */
+    public function materialize_dematerialize_return()
+    {
+        $xs = $this->createHotObservable([
+            onNext(150, 1),
+            onNext(210, 2),
+            onCompleted(250)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->materialize()->dematerialize();
+        });
+
+        $this->assertMessages([
+            onNext(210, 2),
+            onCompleted(250)
+        ], $results->getMessages());
+    }
+
+    /**
+     * @test
+     * @requires function Rx\Observable::dematerialize
+     */
+    public function materialize_dematerialize_throw()
+    {
+        $error = new \Exception();
+
+        $xs = $this->createHotObservable([
+            onNext(150, 1),
+            onError(250, $error)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->materialize()->dematerialize();
+        });
+
+        $this->assertMessages([
+            onError(250, $error)
         ], $results->getMessages());
     }
 }
