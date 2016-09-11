b254d060b6eb80824b0f299d4aacab02f6550ef9
David Dan <davidwdan@gmail.com>
2016-09-10 23:29:18 -0400

    Added subsequent and error on first tests

--- test/Rx/Functional/Operator/SwitchFirstTest.php
+++ test/Rx/Functional/Operator/SwitchFirstTest.php
@@ -110,6 +110,56 @@ class SwitchFirstTest extends FunctionalTestCase
         );
     }
 
+
+    /**
+     * @test
+     */
+    public function switchFirst_first_inner_throws()
+    {
+
+        $error = new \Exception();
+
+        $xs = $this->createHotObservable([
+            onNext(300, $this->createColdObservable([
+                onNext(10, 101),
+                onNext(20, 102),
+                onNext(110, 103),
+                onNext(120, 104),
+                onError(130, $error)
+            ])),
+            onNext(400, $this->createColdObservable([
+                onNext(10, 201),
+                onNext(20, 202),
+                onNext(30, 203),
+                onNext(40, 204),
+                onCompleted(50)
+            ])),
+            onNext(500, $this->createColdObservable([
+                onNext(10, 301),
+                onNext(20, 302),
+                onNext(30, 303),
+                onNext(40, 304),
+                onCompleted(150)
+            ])),
+            onCompleted(600)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->switchFirst();
+        });
+
+        $this->assertMessages(
+            [
+                onNext(310, 101),
+                onNext(320, 102),
+                onNext(410, 103),
+                onNext(420, 104),
+                onError(430, $error)
+            ],
+            $results->getMessages()
+        );
+    }
+
     /**
      * @test
      */
@@ -308,6 +358,53 @@ class SwitchFirstTest extends FunctionalTestCase
                 onNext(510, 105),
                 onNext(520, 106),
                 onCompleted(630)
+            ],
+            $results->getMessages()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function switchFirst_Subsequent()
+    {
+
+        $xs = $this->createHotObservable([
+            onNext(300, $this->createColdObservable([
+                onNext(10, 101),
+                onNext(20, 102),
+                onCompleted(40)
+            ])),
+            onNext(400, $this->createColdObservable([
+                onNext(10, 201),
+                onNext(20, 202),
+                onNext(110, 203),
+                onNext(120, 204),
+                onCompleted(150)
+            ])),
+            onNext(500, $this->createColdObservable([
+                onNext(10, 301),
+                onNext(20, 302),
+                onNext(30, 303),
+                onNext(40, 304),
+                onCompleted(150)
+            ])),
+            onCompleted(600)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->switchFirst();
+        });
+
+        $this->assertMessages(
+            [
+                onNext(310, 101),
+                onNext(320, 102),
+                onNext(410, 201),
+                onNext(420, 202),
+                onNext(510, 203),
+                onNext(520, 204),
+                onCompleted(600)
             ],
             $results->getMessages()
         );
