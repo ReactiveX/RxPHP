243a677063d031be929772a3242860a139e97a6b
David Dan <davidwdan@gmail.com>
2016-09-10 21:17:06 -0400

    Added an additional test

--- test/Rx/Functional/Operator/SwitchFirstTest.php
+++ test/Rx/Functional/Operator/SwitchFirstTest.php
@@ -261,4 +261,55 @@ class SwitchFirstTest extends FunctionalTestCase
             $results->getMessages()
         );
     }
+
+    /**
+     * @test
+     */
+    public function switchFirst_Inner_Completes_Last()
+    {
+
+        $xs = $this->createHotObservable([
+            onNext(300, $this->createColdObservable([
+                onNext(10, 101),
+                onNext(20, 102),
+                onNext(110, 103),
+                onNext(120, 104),
+                onNext(210, 105),
+                onNext(220, 106),
+                onCompleted(330)
+            ])),
+            onNext(400, $this->createColdObservable([
+                onNext(10, 201),
+                onNext(20, 202),
+                onNext(30, 203),
+                onNext(40, 204),
+                onCompleted(50)
+            ])),
+            onNext(500, $this->createColdObservable([
+                onNext(10, 301),
+                onNext(20, 302),
+                onNext(30, 303),
+                onNext(40, 304),
+                onCompleted(150)
+            ])),
+            onCompleted(600)
+        ]);
+
+        $results = $this->scheduler->startWithCreate(function () use ($xs) {
+            return $xs->switchFirst();
+        });
+
+        $this->assertMessages(
+            [
+                onNext(310, 101),
+                onNext(320, 102),
+                onNext(410, 103),
+                onNext(420, 104),
+                onNext(510, 105),
+                onNext(520, 106),
+                onCompleted(630)
+            ],
+            $results->getMessages()
+        );
+    }
 }
