1a01cbdaf6c78717ef98c2eb5668e1d65938a2d6
Cees-Jan <ceesjank@gmail.com>
2016-12-24 14:26:23 +0100

    Custom operator demo

--- demo/bootstrap.php
+++ demo/bootstrap.php
@@ -11,6 +11,7 @@
 
 if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
     $autoload = require_once $file;
+    $autoload->addPsr4('Vendor\\Rx\\Operator\\', __DIR__ . '/custom-operator');
 } else {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
