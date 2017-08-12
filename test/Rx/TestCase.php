04a0d6258117931841f030562f03558e325c38ad
David Dan <davidwdan@gmail.com>
2017-08-11 20:26:45 -0400

    Update TestCase.php

--- test/Rx/TestCase.php
+++ test/Rx/TestCase.php
@@ -4,8 +4,6 @@ declare(strict_types = 1);
 
 namespace Rx;
 
-use PHPUnit_Framework_TestCase;
-
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
 }
